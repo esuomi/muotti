e5081865a9d69201f83fd67bc36558197398ad78
Esko Suomi <esko.suomi@solita.fi>
2022-09-27 16:01:05 +0300

    remove cruft from muotti.dev

--- src/dev/clj/muotti/dev.clj
+++ src/dev/clj/muotti/dev.clj
@@ -4,26 +4,5 @@
             [muotti.core :as muotti]
             [muotti.malli :as mm]))
 
-(def adjacencies {[:keyword :string]  {:validator   keyword?
-                                       :transformer name}
-                  [:keyword :boolean] {:validator   keyword?
-                                       :transformer boolean}
-                  [:string :number]   {:validator   string?
-                                       :transformer parse-long}
-                  [:string :boolean]  {:validator   string?
-                                       :transformer boolean}
-                  [:number :keyword]  {:validator   number?
-                                       :transformer (comp keyword str)}
-                  [:number :boolean]  {:validator   number?
-                                       :transformer boolean}})
-
-#_((malli/decoder
-   [:map
-    [:x [:set [:enum {:muotti/default "S"} "S" "L"]]]
-    [:y {:muotti/ignore true} :uuid]
-    [:z [:tuple :boolean [:map [:a :int]]]]]
-   (mm/transformer (->transformer mm/malli-config)))
- {:x #{"S" "L" "XL" nil}
-  :y :invalid
-  :z [true {:a 123}]})
-; TODO: document :validator being optional
+; 1) start REPL
+; 2) copy-paste code blocks from README.md
